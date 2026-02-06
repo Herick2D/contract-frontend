bc6fd870bc15e1297ae8652429dca6b9ded40a12
Herick2D <contato.herickmoreira@gmail.com>
2026-02-06 10:28:13 -0300

    style: dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    './app*.{js,ts,jsx,tsx,mdx}',
-    './components*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
